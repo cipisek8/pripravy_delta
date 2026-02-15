fd8234a45f3374e6350239842fb5442ce659b64c
cipisek8 <cipisek@pecenka.name>
2026-02-15 11:19:40 +0100

    eslint

--- app/reviews/[id]/accept/route.ts
+++ app/reviews/[id]/accept/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { changeReview } from '@/app/actions/changeReview'
 
-export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
-await changeReview((await params).id, true);
-const url = req.nextUrl.clone();
-url.pathname = `/preparations`;
-return NextResponse.redirect(url, 302);
+export async function POST(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
+
+	await changeReview(id, true)
+
+	const url = req.nextUrl.clone()
+	url.pathname = '/preparations'
+	return NextResponse.redirect(url, 302)
 }
 
